f5f310e800f57a67d20dc080ac17eb680f2b2a66
guswjd6476 <sspp4555@naver.com>
2023-06-09 14:10:21 +0900

    0609

--- DB.js
+++ DB.js
@@ -28,12 +28,25 @@ let connection = mysql.createConnection({
     // connection.connect();
   
     module.exports = connection;
-
-    connection.connect(function(err) {
-      if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-      }
+    function handleDisconnect() {// Recreate the connection, since
+                                                      // the old one cannot be reused.
     
-      console.log('connected as id ' + connection.threadId);
-    });
+      connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+      });                                     // process asynchronous requests in the meantime.
+                                              // If you're also serving http, display a 503 error.
+      connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+      });
+    }
+    
+    handleDisconnect();
+     
