49e15b2e4f000f595823964acae4b369e5d2c5a7
guswjd6476 <sspp4555@naver.com>
2023-06-09 16:23:06 +0900

    0609

--- DB.js
+++ DB.js
@@ -18,35 +18,22 @@
 
 const mysql = require("mysql");
 let connection = mysql.createConnection({
-    host     : 'my8002.gabiadb.com', //실제로 연결할 데이터베이스의 위치
-    user     : 'guswjd6476',
+    host     : 'svc.sel4.cloudtype.app', //실제로 연결할 데이터베이스의 위치
+    user     : 'root',
     password : 'rkdguswjd!!11',
-    database : 'hyuny' //데이터베이스 이름
+    database : 'partshop' //데이터베이스 이름
   });
   
   
     // connection.connect();
   
     module.exports = connection;
-    function handleDisconnect() {// Recreate the connection, since
-                                                      // the old one cannot be reused.
+
+    connection.connect(function(err) {
+      if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+      }
     
-      connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
-          console.log('error when connecting to db:', err);
-          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-      });                                     // process asynchronous requests in the meantime.
-                                              // If you're also serving http, display a 503 error.
-      connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-          handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-          throw err;                                  // server variable configures this)
-        }
-      });
-    }
-    
-    handleDisconnect();
-     
+      console.log('connected as id ' + connection.threadId);
+    });
