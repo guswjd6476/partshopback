d10cc26bb0dcf7a1ea58ebee0264dd0e280379b1
guswjd6476 <sspp4555@naver.com>
2023-06-09 11:29:16 +0900

    0609

--- DB.js
+++ DB.js
@@ -20,7 +20,7 @@ const mysql = require("mysql");
 let connection = mysql.createConnection({
     host     : 'my8002.gabiadb.com', //실제로 연결할 데이터베이스의 위치
     user     : 'guswjd6476',
-    password : 'rkdguswjd--11',
+    password : 'rkdguswjd!!11',
     database : 'hyuny' //데이터베이스 이름
   });
   
