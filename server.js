52f0157645e556a668171bed448287b838c8d57b
guswjd6476 <sspp4555@naver.com>
2023-06-13 09:40:52 +0900

    0613

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-app.listen(80, function () {
+app.listen('https://web-partshop-lme62alhk7lvdw.sel4.cloudtype.app', function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
