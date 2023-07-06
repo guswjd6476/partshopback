f2460b07963754cd826c1e2253ecb17cb95654f9
guswjd6476 <sspp4555@naver.com>
2023-07-06 15:38:26 +0900

    0706

--- server.js
+++ server.js
@@ -882,7 +882,7 @@ app.get('/api/getThisAfterbuylist', (req, res) => {
 // 운송장등록 /api/addDeliver
 app.get('/api/addDeliver', (req, res) => {
   db.query('UPDATE buylist SET buystate = ?, dNum = ? ,carrier = ? WHERE id = ? ', [req.query.state,req.query.dNum,req.query.carriers,req.query.id], (error, results, fields) => {
-   
+   console.log(req.query.state,req.query.dNum,req.query.carriers,req.query.id)
     if (error) {
       // 오류 처리
       console.error(error);
