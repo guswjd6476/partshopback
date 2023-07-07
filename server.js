39dfbd6700f380cc520b596c797407ccabe86e9c
guswjd6476 <sspp4555@naver.com>
2023-07-07 15:43:08 +0900

    0707

--- server.js
+++ server.js
@@ -899,7 +899,7 @@ app.get('/api/addDeliver', (req, res) => {
 // 찜목록 가져오기
 app.get('/api/getNeeds', (req, res) => {
   db.query('SELECT p.*,a.*,c.category,pd.subcategory FROM needs AS a INNER JOIN productlist AS p ON a.productnum = p.id  JOIN category AS c ON  p.catenum = c.catenum JOIN productdata AS pd ON  p.catenum = c.catenum  WHERE a.userId = ?',[req.query.userId], (error, results, fields) => {
-    console.log(userId,'userId')
+    console.log(req.query.userId,'userId')
     res.status(200).send(results);
  
 })
